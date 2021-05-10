38a3187ac2215d5ae0ef0d44fc05097c50545652
lyh <397432978@qq.com>
2021-05-10 15:54:24 +0800

    V3

--- run_som-dst.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES="0,1" python train.py \
-                                    --data_root data/mwz2.1/ \
-                                    --op_code '4' \
-                                    --batch_size 32 \
-                                    --grad_accumulation 1 \
-                                    --dec_lr 1e-4\
